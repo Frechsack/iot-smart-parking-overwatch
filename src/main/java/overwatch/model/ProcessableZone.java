eff8ef0cb9edc30ce0dd9474a78f3fb9ff5f5698
frechsack <patrickwolter.1@gmx.de>
2023-01-16 22:45:47 +0100

    Erweiterung von Zone durch ProcessableZone.

--- src/main/java/overwatch/model/ProcessableZone.java
+++ src/main/java/overwatch/model/ProcessableZone.java
@@ -3,7 +3,7 @@ package overwatch.model;
 import overwatch.Image;
 import overwatch.service.ImageService;
 
-public class ProcessedZone {
+public class ProcessableZone extends Zone {
 
     private static final short UNSET = 0;
     private static final short MODIFIED = 1;
@@ -12,17 +12,21 @@ public class ProcessedZone {
     private static final float BRIGHTNESS_DELTA = 0.10f;
 
     private final short [] pixelStates;
-    public final Zone zone;
 
-
-    public ProcessedZone(Zone zone) {
-        this.zone = zone;
-        this.pixelStates = new short[zone.width * zone.height];
-
+    public ProcessableZone(Zone zone) {
+        super(zone);
+        this.pixelStates = new short[width * height];
     }
 
+    public ProcessableZone(int nr, int offsetX, int offsetY, int width, int height, Capture capture) {
+        super(nr, offsetX, offsetY, width, height, capture);
+        this.pixelStates = new short[width * height];
+    }
+
+
+
     public boolean isModified(int x, int y){
-        int index = (x-1) + (y-1) * zone.width;
+        int index = (x-1) + (y-1) * width;
         if (pixelStates[index] == UNSET){
             return processedPixel(x,y,index);
 
@@ -35,13 +39,13 @@ public class ProcessedZone {
             return pixelStates[index] == MODIFIED;
         }
 
-        Image sourceImage = ImageService.readSourceImage(zone.capture);
-        Image currentImage = ImageService.readCurrentImage(zone.capture);
+        Image sourceImage = ImageService.readSourceImage(capture);
+        Image currentImage = ImageService.readCurrentImage(capture);
 
-        int offSetX = zone.offsetX + x;
-        int offSetY = zone.offsetY + y;
-        int sourcePixel = sourceImage.getPixel(offSetX, offSetY);
-        int currentPixel = currentImage.getPixel(offSetX, offSetY);
+        int offsetX = this.offsetX + x;
+        int offsetY = this.offsetY + y;
+        int sourcePixel = sourceImage.getPixel(offsetX, offsetY);
+        int currentPixel = currentImage.getPixel(offsetX, offsetY);
 
         float sourceBrightness = calculateBrightness(sourcePixel);
         float currentBrightness = calculateBrightness(currentPixel);
@@ -59,9 +63,9 @@ public class ProcessedZone {
         int b = rgb & 0xff;
         int g = (rgb & 0xff00) >> 8;
 
-        int cmax = (r > g) ? r : g;
+        int cmax = Math.max(r, g);
         if (b > cmax) cmax = b;
-        int cmin = (r < g) ? r : g;
+        int cmin = Math.min(r, g);
         if (b < cmin) cmin = b;
 
         return ((float) cmax) / 255.0f;
--- src/main/java/overwatch/model/Zone.java
+++ src/main/java/overwatch/model/Zone.java
@@ -9,6 +9,18 @@ public class Zone {
     public final int height;
     public final Capture capture;
 
+    /*
+    * Calculated
+    */
+    public final int absoluteXStart;
+    public final int absoluteXEnd;
+    public final int absoluteYStart;
+    public final int absoluteYEnd;
+
+    public Zone(Zone copy) {
+        this(copy.nr, copy.offsetX, copy.offsetY, copy.width, copy.height, copy.capture);
+    }
+
     public Zone(int nr, int offsetX, int offsetY, int width, int height, Capture capture) {
         this.nr = nr;
         this.offsetX = offsetX;
@@ -16,11 +28,13 @@ public class Zone {
         this.width = width;
         this.height = height;
         this.capture = capture;
+        absoluteXStart = offsetX + capture.x;
+        absoluteYStart = offsetY + capture.y;
 
+
+        absoluteXEnd = absoluteXStart + width - 1;
+        absoluteYEnd = absoluteYStart + height - 1;
     }
-
-
-
 }
 
 
