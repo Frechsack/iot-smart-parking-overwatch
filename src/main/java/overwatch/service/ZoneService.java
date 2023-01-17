f1baf160766f306e06ebe3972d474385310ddc06
frechsack <patrickwolter.1@gmx.de>
2023-01-17 23:12:31 +0100

    Doc.

--- src/main/java/overwatch/service/ZoneService.java
+++ src/main/java/overwatch/service/ZoneService.java
@@ -1,35 +1,73 @@
 package overwatch.service;
 
+import org.jetbrains.annotations.Nullable;
 import overwatch.model.ProcessableZone;
 import overwatch.model.Zone;
 
+/**
+ * Statische Methoden für die Berechnung von Zonen übergreifenden Operationen.
+ */
 public class ZoneService {
 
+    /**
+     * Die maximale Überschneidung bzw. Abstand zwischen zwei Zonen damit diese berührend erkannt werden.
+     */
+    private static final int INTERSECTION_DELTA = 5;
+
+    /**
+     * Der Zustand eines Pixels.
+     */
     public enum PixelState {
 
-        MODIFIED(true, false),
+        /**
+         * Der Pixel wurde gefunden und modifiziert.
+         */
+        MODIFIED(true, false, true),
 
-        UNMODIFIED(false, false),
+        /**
+         * Der Pixel wurde gefunden und nicht modifiziert.
+         */
+        UNMODIFIED(false, false, true),
 
-        MODIFIED_ZONE_CHANGE(true, true),
+        /**
+         * Der Pixel wurde gefunden und modifiziert. Der Pixel wurde außerdem in einer anderen Zone gefunden, als angenommen.
+         */
+        MODIFIED_ZONE_CHANGE(true, true, true),
 
-        UNMODIFIED_ZONE_CHANGE(false, true);
+        /**
+         * Der Pixel wurde gefunden und nicht modifiziert. Der Pixel wurde außerdem in einer anderen Zone gefunden, als angenommen.
+         */
+        UNMODIFIED_ZONE_CHANGE(false, true, true),
+
+        /**
+         * Der Pixel wurde in keiner Zone gefunden.
+         */
+        NOT_EXISTING(false, false, false);
 
         public final boolean isModified;
 
         public final boolean isZoneChanged;
 
-        PixelState(boolean isModified, boolean isZoneChanged){
+        public final boolean isExisting;
+
+        PixelState(boolean isModified, boolean isZoneChanged, boolean isExisting){
             this.isZoneChanged = isZoneChanged;
             this.isModified = isModified;
+            this.isExisting = isExisting;
         }
     }
 
-    private static final int INTERSECTION_DELTA = 5;
-
     private ZoneService () {}
 
-
+    /**
+     * Errechnet die Zone zu einem Pixel.
+     * @param absoluteX Die absolute Position des Pixel auf der x-Achse.
+     * @param absoluteY Die absolute Position des Pixel auf der y-Achse.
+     * @param zones Die zu analysierenden Zonen.
+     * @return Gibt die errechnete Zone zurück..
+     * @param <E> Der class-type.
+     * @throws IllegalArgumentException Wirft eine {@link IllegalArgumentException}, sollte keine Zone gefunden werden.
+     */
     public static <E extends Zone> E findZoneForPixel(final int absoluteX, final int absoluteY, final E[] zones ){
         for(E zone : zones) {
             if (zone.absoluteXStart <= absoluteX && zone.absoluteXEnd >= absoluteX && zone.absoluteYStart <= absoluteY && zone.absoluteYEnd >= absoluteY)
@@ -46,9 +84,8 @@ public class ZoneService {
      * @param zones Die auszuwertenden Zonen.
      * @param shortcut Eine optionale Zone, die zuerst durchsucht werden soll.
      * @return Gibt zurück, ob der Pixel mutiert wurde oder nicht.
-     * @param <E> Class-type von {@link Zone}.
      */
-    public static PixelState calculatePixelState(final int absoluteX, final int absoluteY, final ProcessableZone[] zones, final ProcessableZone shortcut){
+    public static PixelState calculatePixelState(final int absoluteX, final int absoluteY, final ProcessableZone[] zones,@Nullable final ProcessableZone shortcut){
         if(shortcut != null && shortcut.absoluteXStart <= absoluteX && shortcut.absoluteXEnd >= absoluteX && shortcut.absoluteYStart <= absoluteY && shortcut.absoluteYEnd >= absoluteY)
             return shortcut.isModified(absoluteX - shortcut.absoluteXStart, absoluteY - shortcut.absoluteYStart)
                 ? PixelState.MODIFIED
@@ -60,11 +97,19 @@ public class ZoneService {
                         ? PixelState.MODIFIED_ZONE_CHANGE
                         : PixelState.UNMODIFIED_ZONE_CHANGE;
         }
-        throw new IllegalArgumentException("There is no matching zone for coordinates x: '" + absoluteX + "' y: '" + absoluteY + "'");
+        return PixelState.NOT_EXISTING;
     }
 
-    public static <E extends Zone> E findZoneNorthOf(E currentZone, int x, E[] zones) {
-        final int absoluteX = currentZone.absoluteXStart + x;
+    /**
+     * Findet eine Zone, welche Nördlich der übergebenen Zone liegt.
+     * @param currentZone Die ausgangs Zone.
+     * @param relativeX Die Position auf der x-Achse. Die Nördlich liegende Zone muss die x-Achse an dieser Koordinate schneiden.
+     * @param zones Die zu analysierenden Zonen.
+     * @return Gibt die gefundene Zone oder null zurück, sollte keine Zone gefunden werden.
+     * @param <E> Der Zone class-type.
+     */
+    public static <E extends Zone> E findZoneNorthOf(E currentZone, int relativeX, E[] zones) {
+        final int absoluteX = currentZone.absoluteXStart + relativeX;
         assert absoluteX <= currentZone.absoluteXEnd;
         for (E zone : zones){
             if(zone == currentZone) continue;
@@ -76,8 +121,16 @@ public class ZoneService {
         return null;
     }
 
-    public static <E extends Zone> E findZoneSouthOf(E currentZone, int x, E[] zones) {
-        final int absoluteX = currentZone.absoluteXStart + x;
+    /**
+     * Findet eine Zone, welche Südlich der übergebenen Zone liegt.
+     * @param currentZone Die ausgangs Zone.
+     * @param relativeX Die Position auf der x-Achse. Die Südlich liegende Zone muss die x-Achse an dieser Koordinate schneiden.
+     * @param zones Die zu analysierenden Zonen.
+     * @return Gibt die gefundene Zone oder null zurück, sollte keine Zone gefunden werden.
+     * @param <E> Der Zone class-type.
+     */
+    public static <E extends Zone> E findZoneSouthOf(E currentZone, int relativeX, E[] zones) {
+        final int absoluteX = currentZone.absoluteXStart + relativeX;
         assert absoluteX <= currentZone.absoluteXEnd;
 
         for (E zone : zones){
@@ -90,8 +143,16 @@ public class ZoneService {
         return null;
     }
 
-    public static <E extends Zone> E findZoneEastOf(E currentZone, int y, E[] zones) {
-        final int absoluteY = currentZone.absoluteYStart + y;
+    /**
+     * Findet eine Zone, welche Östlich der übergebenen Zone liegt.
+     * @param currentZone Die ausgangs Zone.
+     * @param relativeY Die Position auf der x-Achse. Die Östlich liegende Zone muss die x-Achse an dieser Koordinate schneiden.
+     * @param zones Die zu analysierenden Zonen.
+     * @return Gibt die gefundene Zone oder null zurück, sollte keine Zone gefunden werden.
+     * @param <E> Der Zone class-type.
+     */
+    public static <E extends Zone> E findZoneEastOf(E currentZone, int relativeY, E[] zones) {
+        final int absoluteY = currentZone.absoluteYStart + relativeY;
         assert absoluteY <= currentZone.absoluteYEnd;
 
         for(E zone : zones) {
@@ -104,8 +165,16 @@ public class ZoneService {
         return null;
     }
 
-    public static <E extends Zone> E findZoneWestOf(E currentZone, int y, E[] zones) {
-        final int absoluteY = currentZone.absoluteYStart + y;
+    /**
+     * Findet eine Zone, welche Westlich der übergebenen Zone liegt.
+     * @param currentZone Die ausgangs Zone.
+     * @param relativeY Die Position auf der x-Achse. Die Westlich liegende Zone muss die x-Achse an dieser Koordinate schneiden.
+     * @param zones Die zu analysierenden Zonen.
+     * @return Gibt die gefundene Zone oder null zurück, sollte keine Zone gefunden werden.
+     * @param <E> Der Zone class-type.
+     */
+    public static <E extends Zone> E findZoneWestOf(E currentZone, int relativeY, E[] zones) {
+        final int absoluteY = currentZone.absoluteYStart + relativeY;
         assert absoluteY <= currentZone.absoluteYEnd;
 
         for(E zone : zones) {
