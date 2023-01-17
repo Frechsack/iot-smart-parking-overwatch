177b0daaf1a46f5de1d6f883ee76729843e8f0ed
frechsack <patrickwolter.1@gmx.de>
2023-01-17 23:27:49 +0100

    Doc.

--- src/main/java/overwatch/model/Zone.java
+++ src/main/java/overwatch/model/Zone.java
@@ -1,20 +1,58 @@
 package overwatch.model;
 
+/**
+ * Eine virtuelle Unterteilung eines Bildes.
+ */
 public class Zone {
 
+    /**
+     * Die Nr der Zone.
+     */
     public final int nr;
+
+    /**
+    Die Positionierung der Zone auf der x-Achse innerhalb seiner {@link Capture}.
+    */
     public final int offsetX;
+
+    /**
+     Die Positionierung der Zone auf der y-Achse innerhalb seiner {@link Capture}.
+     */
     public final int offsetY;
+
+    /**
+     * Die Breite der Zone auf der x-Achse.
+     */
     public final int width;
+
+    /**
+     * Die Höhe der Zone auf der y-Achse.
+     */
     public final int height;
+
+    /**
+     * Das zugewiesene {@link Capture}.
+     */
     public final Capture capture;
 
-    /*
-    * Calculated
-    */
+    /**
+     * Die kleinste Koordinate auf der x-Achse in absoluten Koordinaten.
+     */
     public final int absoluteXStart;
+
+    /**
+     * Die größte Koordinate auf der x-Achse in absoluten Koordinaten.
+     */
     public final int absoluteXEnd;
+
+    /**
+     * Die kleinste Koordinate auf der y-Achse in absoluten Koordinaten.
+     */
     public final int absoluteYStart;
+
+    /**
+     * Die größte Koordinate auf der y-Achse in absoluten Koordinaten.
+     */
     public final int absoluteYEnd;
 
     public Zone(Zone copy) {
@@ -30,10 +68,13 @@ public class Zone {
         this.capture = capture;
         absoluteXStart = offsetX + capture.x;
         absoluteYStart = offsetY + capture.y;
-
-
         absoluteXEnd = absoluteXStart + width - 1;
         absoluteYEnd = absoluteYStart + height - 1;
+
+        if(this.width > capture.width)
+            throw new IllegalStateException("Zone is widder than it´s capture.");
+        if(this.height > capture.height)
+            throw new IllegalStateException("Zone is higher than it´s capture.");
     }
 }
 
