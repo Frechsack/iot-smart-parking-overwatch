771c5566377ff54c638692d3cd07a810400bbdcc
frechsack <patrickwolter.1@gmx.de>
2023-01-17 23:27:45 +0100

    Doc.

--- src/main/java/overwatch/model/ProcessableZone.java
+++ src/main/java/overwatch/model/ProcessableZone.java
@@ -5,17 +5,34 @@ import overwatch.service.ImageService;
 
 import java.util.Arrays;
 
+/**
+ * Eine spezialisierte Variante von {@link Zone}, welche Pixeldaten beinhält.
+ */
 public class ProcessableZone extends Zone {
 
     /**
      * Wichtig: Muss null entsprechen, weil null als default für shorts gilt und auf diese Logik zurückgegriffen wird.
      */
     private static final short UNSET = 0;
+
+    /**
+     * Ein Pixel ist modifiziert.
+     */
     private static final short MODIFIED = 1;
+
+    /**
+     * Ein Pixel ist nicht modifiziert.
+     */
     private static final short UNMODIFIED = 2;
 
+    /**
+     * Der zu erreichende Schwellwert, damit zwei Pixel als verschieden und somit {@link #MODIFIED} gelten.
+     */
     private static final float BRIGHTNESS_DELTA = 0.01f;
 
+    /**
+     * Die aktuellen Status der Pixel.
+     */
     private final short [] pixelStates;
 
     public ProcessableZone(Zone zone) {
@@ -28,18 +45,34 @@ public class ProcessableZone extends Zone {
         this.pixelStates = new short[width * height];
     }
 
+    /**
+     * Setzt alle Pixeldaten zurück. Sie nehmen den Zustand {@link #UNSET} an.
+     */
     public void reset(){
         Arrays.fill(pixelStates, (short) 0);
     }
 
-    public boolean isModified(int x, int y){
-        int index = x + y * width;
+    /**
+     * Prüft, ob ein Pixel modifiziert wurde.
+     * @param relativeX Die Position auf der x-Achse.
+     * @param relativeY Die Position auf der y-Achse.
+     * @return Gibt {@code true} zurück, sollte der Pixel modifiziert sein, andernfalls {@code false}.
+     */
+    public boolean isModified(int relativeX, int relativeY){
+        int index = relativeX + relativeY * width;
         if (pixelStates[index] == UNSET){
-            return processPixel(x,y,index);
+            return processPixel(relativeX,relativeY,index);
         }
         return pixelStates[index] == MODIFIED;
     }
 
+    /**
+     * Berechnung eines Pixelzustandes und Ausgabe, ob dieser {@link #MODIFIED} ist.
+     * @param x Die Position auf der x-Achse.
+     * @param y Die Position auf der y-Achse.
+     * @param index Der berechnete Index für die Speicherposition des Pixels.
+     * @return Gibt aus, ob der Pixel {@link #MODIFIED} oder {@link #UNMODIFIED} ist.
+     */
     private boolean processPixel(int x, int y, int index){
         if (pixelStates[index] != UNSET) {
             return pixelStates[index] == MODIFIED;
@@ -71,8 +104,6 @@ public class ProcessableZone extends Zone {
 
         int cmax = Math.max(r, g);
         if (b > cmax) cmax = b;
-        int cmin = Math.min(r, g);
-        if (b < cmin) cmin = b;
 
         return ((float) cmax) / 255.0f;
     }
