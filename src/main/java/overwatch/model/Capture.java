56a504fc6c8be9c60e9527f4e0bdec7b18ee734a
frechsack <patrickwolter.1@gmx.de>
2023-01-17 23:27:52 +0100

    Doc.

--- src/main/java/overwatch/model/Capture.java
+++ src/main/java/overwatch/model/Capture.java
@@ -1,10 +1,29 @@
 package overwatch.model;
 
-public class Capture {
+/**
+ * Stellt ein Videogerät dar, von welchen ein Bild gemacht werden kann.
+ */
+@SuppressWarnings("ClassCanBeRecord")
+public final class Capture {
 
+    /**
+     * Die virtuelle Positionierung des Bildes im Parkhaus auf der x-Achse.
+     */
     public final int x;
+
+    /**
+     * Die virtuelle Positionierung des Bildes im Parkhaus auf der y-Achse.
+     */
     public final int y;
+
+    /**
+     * Die Breite des Bildes.
+     */
     public final int width;
+
+    /**
+     * Die Höhe des Bildes.
+     */
     public final int height;
 
     public Capture(int x, int y, int width, int height, String deviceName) {
@@ -15,5 +34,8 @@ public class Capture {
         this.deviceName = deviceName;
     }
 
+    /**
+     * Der Name des Videogerätes.
+     */
     public final String deviceName;
 }
