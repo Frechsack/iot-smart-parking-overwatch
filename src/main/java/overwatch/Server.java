8a6b0196141fe940736f28401e2bcb781cdd184a
Frechsack <patrick.wolter@pwolter.de>
2023-01-23 21:05:03 +0100

    Http-Server.

--- src/main/java/overwatch/Server.java
+++ src/main/java/overwatch/Server.java
@@ -1,38 +1,75 @@
 package overwatch;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.iki.elonen.NanoHTTPD;
+import overwatch.dto.InitDto;
 import overwatch.service.ConfigurationService;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class Server extends NanoHTTPD {
+
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
     @Override
     public Response serve(IHTTPSession session) {
         if (session.getMethod() != Method.POST)
             return createError(405, "Method Not Allowed");
-        String payload = readFromInputStream(session.getInputStream());
-        System.out.print(payload);
+        Optional<InitDto> requestOptional = readInitRequestFromSession(session);
+        if(requestOptional.isEmpty())
+            return createError(409, "Empty data");
+        InitDto request = requestOptional.get();
+        Engine.start(request.toZones());
         return createSuccessful();
     }
-    private Response createError(int status, String message){
-        return newFixedLengthResponse(new HTTPStatus(status), NanoHTTPD.MIME_PLAINTEXT, message);
-    }
-    private String readFromInputStream(InputStream stream){
-        String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining());
-        return text;
+
+    private static Optional<InitDto> readInitRequestFromSession(IHTTPSession session){
+        return readPayloadFromSession(session)
+                .flatMap(payload -> {
+                   ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        return Optional.of(mapper.readValue(payload, InitDto.class));
+                    } catch (JsonProcessingException e) {
+                        logger.severe(e.getMessage());
+                        return Optional.empty();
+                    }
+                });
     }
 
-    private Response createSuccessful(){
+    private static Optional<String> readPayloadFromSession(IHTTPSession session) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            session.parseBody(map);
+        }
+        catch (Exception e){
+            logger.severe(e.getMessage());
+        }
+        return Optional.ofNullable(map.get("postData"));
+    }
+
+    private static Response createError(int status, String message){
+        return newFixedLengthResponse(new HTTPStatus(status), NanoHTTPD.MIME_PLAINTEXT, message);
+    }
+
+    private static Response createSuccessful(){
         return newFixedLengthResponse(new HTTPStatus(200), NanoHTTPD.MIME_PLAINTEXT, "");
     }
 
     public Server() {
         super(ConfigurationService.getInt(ConfigurationService.Keys.SERVER_PORT));
     }
+
+    @Override
+    public void start() throws IOException {
+        this.start(-1, false);
+        logger.info("Server is running");
+    }
+
     private static class HTTPStatus implements Response.IStatus {
         private final int statusCode;
 
