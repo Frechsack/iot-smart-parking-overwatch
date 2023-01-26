47f5b17cef7a30912485886e434024eab79e54eb
frechsack <patrickwolter.1@gmx.de>
2023-01-26 21:26:47 +0100

    Testanpassungen

--- src/main/java/overwatch/DebugApplication.java
+++ src/main/java/overwatch/DebugApplication.java
@@ -8,32 +8,22 @@ import overwatch.service.ImageService;
 public class DebugApplication {
 
     public static void main(String[] args) {
-        Capture c1 = new Capture(0,0,800,400, Capture.VIRTUAL_CAMERA_NAME);
-        Zone z1 = new Zone(1, c1, 0,0, 200,200);
-        Zone z2 = new Zone(2, c1, 200,0, 200,200);
-        Zone z3 = new Zone(3, c1, 400,0, 200,200);
-        Zone z4 = new Zone(4, c1,600,0, 200,200);
-        Zone z5 = new Zone(5, c1,0,200, 200,200);
-        Zone z6 = new Zone(6, c1,200,200, 200,200);
-        Zone z7 = new Zone(7, c1,400,200, 200,200);
-        Zone z8 = new Zone(8, c1,600,200, 200,200);
+        Capture c1 = new Capture(0,0,352,288, "/dev/video0");
+        Zone z1 = new Zone(1, c1,   0,  0, 100,150);
+        Zone z2 = new Zone(2, c1, 100,  0, 100,150);
+        Zone z3 = new Zone(3, c1, 200,  0, 100,150);
+        Zone z4 = new Zone(4, c1, 300,  0,  52,150);
 
-
-        Capture c2 = new Capture(0,400,800,400, Capture.VIRTUAL_CAMERA_NAME);
-        Zone z9 = new Zone(9, c2, 0,0, 200,200);
-        Zone z10 = new Zone(10, c2, 200,0, 200,200);
-        Zone z11 = new Zone(11, c2, 400,0, 200,200);
-        Zone z12 = new Zone(12, c2,600,0, 200,200);
-        Zone z13 = new Zone(13, c2,0,200, 800,200);
+        Zone z5 = new Zone(5, c1,0,150, 100,138);
+        Zone z6 = new Zone(6, c1,100,150, 100,138);
+        Zone z7 = new Zone(7, c1,200,150, 100,138);
+        Zone z8 = new Zone(7, c1,300,150, 52,138);
 
         Zone[] zones = new Zone[]{
-                z1,z2,z3,z4,z5,z6,z7,z8, z9, z10, z11, z12, z13
+                z1,z2,z3,z4,z5,z6,z7, z8
         };
 
-        ImageService.updateSourceImage(c1);
-        ImageService.updateCurrentImage(c1);
-        ImageService.updateSourceImage(c2);
-        ImageService.updateCurrentImage(c2);
+        Engine.start(zones);
 
         new DebugFrame(zones);
     }
--- src/main/java/overwatch/debug/DebugFrame.java
+++ src/main/java/overwatch/debug/DebugFrame.java
@@ -1,68 +1,41 @@
 package overwatch.debug;
 
-import overwatch.model.ProcessableZone;
+import overwatch.Engine;
 import overwatch.model.Zone;
-import overwatch.service.ObjectAnalyserService;
-import overwatch.service.ZoneService;
+import overwatch.service.ConfigurationService;
 import overwatch.skeleton.Outline;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Collection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DebugFrame extends JFrame {
-    private final Outline outline;
-    private final ProcessableZone[] zones;
 
-    private final Collection<Outline> objects;
-
-    private final Collection<ProcessableZone> zonesWithObject;
-
-    private class RootPanel extends JPanel {
+    private static class RootPanel extends JPanel {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
-            for(ProcessableZone zone : zones) {
-                if(zonesWithObject.stream().anyMatch(it -> it.nr() == zone.nr())){
-                    g.setColor(Color.green);
-                    g.fillRect(zone.x(), zone.y(), zone.width(), zone.height());
-                }
-                g.setColor(Color.BLUE);
-                g.drawRect(zone.x(), zone.y(), zone.width(), zone.height());
-                g.drawString(Integer.toString(zone.nr()), zone.x() + 5, zone.y() + 15);
-            }
-            for(int x = outline.x(); x < outline.width(); x++){
-                for(int y = outline.y(); y < outline.height(); y++){
-                    if(ZoneService.calculatePixelState(x, y, zones, null).isModified) {
-                        g.setColor(Color.black);
-                        g.fillRect(x,y,1,1);
-                    }
-                }
-            }
-
-            g.setColor(Color.RED);
-            for (Outline object: objects){
-                g.drawRect(object.x(), object.y(), object.width(), object.height());
-            }
+           g.drawImage(Engine.getGeneratedImage(),0,0, null);
         }
     }
 
     public DebugFrame (Zone[] zones){
         super();
-        this.outline = Outline.compose(zones);
-        this.zones = new ProcessableZone[zones.length];
-        for (int i = 0; i < zones.length; i++)
-            this.zones[i] = new ProcessableZone(zones[i]);
-
-        this.objects = ObjectAnalyserService.findObjects(this.zones);
-        this.zonesWithObject = ObjectAnalyserService.findZonesWithObject(this.zones, objects);
-
+        Outline outline = Outline.compose(zones);
         setSize(outline.width() + 2, outline.height() + 40);
         RootPanel root = new RootPanel();
         root.setBounds(0, 0, outline.width(), outline.height());
         setContentPane(root);
         setVisible(true);
+
+        Timer timer = new Timer(ConfigurationService.getInt(ConfigurationService.Keys.ANALYSE_INTERVAL_MS), e -> {
+            root.repaint();
+        });
+        timer.setInitialDelay(1000);
+        timer.setRepeats(true);
+        timer.start();
     }
 
 }
