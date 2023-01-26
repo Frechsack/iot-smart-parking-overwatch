c9cfff80c695f744df3df4bc74f088cddc1abfe2
frechsack <patrickwolter.1@gmx.de>
2023-01-26 21:26:22 +0100

    Anpassungen

--- src/main/java/overwatch/BenchmarkRunner.java
+++ src/main/java/overwatch/BenchmarkRunner.java
@@ -18,27 +18,36 @@ public class BenchmarkRunner {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @Warmup(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 10, time = 1000, timeUnit =  TimeUnit.MILLISECONDS)
-    @Fork(value = 2, warmups = 5)
+    @Warmup(iterations = 2, time = 800, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 2, time = 800, timeUnit =  TimeUnit.MILLISECONDS)
+    @Fork(value = 2, warmups = 3)
     public void findObjects(){
 
-        Capture capture = new Capture(0,0,800,400, Capture.VIRTUAL_CAMERA_NAME);
-        Zone z1 = new Zone(1, capture, 0,0, 200,200);
-        Zone z2 = new Zone(2, capture, 200,0, 200,200);
-        Zone z3 = new Zone(3, capture, 400,0, 200,200);
-        Zone z4 = new Zone(4, capture,600,0, 200,200);
-        Zone z5 = new Zone(5, capture,0,200, 200,200);
-        Zone z6 = new Zone(6, capture,200,200, 200,200);
-        Zone z7 = new Zone(7, capture,400,200, 200,200);
-        Zone z8 = new Zone(8, capture,600,200, 200,200);
+        Capture c1 = new Capture(0,0,800,400, Capture.VIRTUAL_CAMERA_NAME);
+        Zone z1 = new Zone(1, c1, 0,0, 200,200);
+        Zone z2 = new Zone(2, c1, 200,0, 200,200);
+        Zone z3 = new Zone(3, c1, 400,0, 200,200);
+        Zone z4 = new Zone(4, c1,600,0, 200,200);
+        Zone z5 = new Zone(5, c1,0,200, 200,200);
+        Zone z6 = new Zone(6, c1,200,200, 200,200);
+        Zone z7 = new Zone(7, c1,400,200, 200,200);
+        Zone z8 = new Zone(8, c1,600,200, 200,200);
+
+        Capture c2 = new Capture(0,400,800,400, Capture.VIRTUAL_CAMERA_NAME);
+        Zone z9 = new Zone(9, c2, 0,0, 200,200);
+        Zone z10 = new Zone(10, c2, 200,0, 200,200);
+        Zone z11 = new Zone(11, c2, 400,0, 200,200);
+        Zone z12 = new Zone(12, c2,600,0, 200,200);
+        Zone z13 = new Zone(13, c2,0,200, 800,200);
 
         Zone[] zones = new Zone[]{
-                z1,z2,z3,z4,z5,z6,z7,z8
+                z1,z2,z3,z4,z5,z6,z7,z8, z9, z10, z11, z12, z13
         };
 
-        ImageService.updateSourceImage(capture);
-        ImageService.updateCurrentImage(capture);
+        ImageService.updateSourceImage(c1);
+        ImageService.updateSourceImage(c2);
+        ImageService.updateCurrentImage(c1);
+        ImageService.updateCurrentImage(c2);
 
         ProcessableZone[] processableZones = new ProcessableZone[zones.length];
         for (int i = 0; i < processableZones.length; i++) {
