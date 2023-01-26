8cb4790526a165db822aaf4f6f1f5d5bc2078749
frechsack <patrickwolter.1@gmx.de>
2023-01-26 21:27:31 +0100

    Aufruf an REST-Endpunkt.

---
+++ src/main/java/overwatch/service/HttpService.java
@@ -0,0 +1,53 @@
+package overwatch.service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class HttpService {
+
+    private static final Logger logger = Logger.getLogger(HttpService.class.getName());
+
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+
+    private static final String updateEndpoint = ConfigurationService.getString(ConfigurationService.Keys.SERVICE_UPDATE_ENDPOINT);
+
+    private HttpService() {}
+
+    public static void sendZoneUpdate(int[] zoneNrs){
+        final Collection<QueryParam> queryParams = new ArrayList<>(zoneNrs.length);
+        for (int zoneNr : zoneNrs)
+            queryParams.add(new QueryParam("takenZones", Integer.toString(zoneNr)));
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(buildUri(queryParams))
+                    .PUT(HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() > 204)
+                logger.severe("Illegal response from service. Response-Status: '"
+                        + response.statusCode()
+                        + "', Response-Body: '"
+                        + response.body() + "'.");
+        }
+        catch (Exception e){
+            logger.severe(e.getMessage());
+        }
+    }
+
+    private static URI buildUri(Collection<QueryParam> queryParams) {
+        final String params = queryParams.stream()
+                .map(queryParam -> queryParam.key + "=" + queryParam.value)
+                .collect(Collectors.joining("&"));
+        return URI.create(HttpService.updateEndpoint + "?" + params);
+
+    }
+
+    private record QueryParam(String key, String value){};
+
+}
