aa19581f519a9ee7875affbc9c559721c85ff5d5
frechsack <patrickwolter.1@gmx.de>
2023-01-27 19:55:39 +0100

    Tests mit OpenCv.

--- src/main/java/overwatch/OpenCvApplication.java
+++ src/main/java/overwatch/OpenCvApplication.java
@@ -1,15 +1,13 @@
 package overwatch;
 
 
-import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.Point;
-import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.opencv.opencv_video.BackgroundSubtractor;
 import org.bytedeco.opencv.opencv_videoio.VideoCapture;
 
 import static org.bytedeco.opencv.global.opencv_highgui.imshow;
 import static org.bytedeco.opencv.global.opencv_highgui.waitKey;
-import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
 import static org.bytedeco.opencv.global.opencv_video.createBackgroundSubtractorMOG2;
 
 public class OpenCvApplication {
@@ -37,9 +35,20 @@ public class OpenCvApplication {
             // get the frame number and write it on the current frame
             rectangle(frame, new Point(10, 2), new Point(100, 20), new Scalar(255, 255, 255,-1));
 
+            MatVector contours = new MatVector();
+
+            findContours(fgMask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+            for (int i = 0; i < contours.size(); i++){
+                Rect contour = boundingRect(contours.get(i));
+                rectangle(fgMask, contour, new Scalar(255,0,0,-1));
+
+            }
+
             // show the current frame and the fg masks
             imshow("Frame", frame);
             imshow("FG Mask", fgMask);
+
             // get the input from the keyboard
             int keyboard = waitKey(30);
             if (keyboard == 'q' || keyboard == 27) {
